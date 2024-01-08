a305a08082faeec303be8e2748b2f4729a0be1f6
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-01-08 20:35:42 +0100

    Added header and footer to flyout. Started implementing Language choices

---
+++ ZHomeLibraryShellApp/Language/English.cs
@@ -0,0 +1,70 @@
+﻿namespace ZHomeLibraryShellApp.Language;
+
+public class English : ILanguage
+{
+    public string YourBooks { get; set; } = "Your books";
+    public string SearchTitlePlaceHolder { get; set; } = "Search for a title";
+    public string SearchBorrowerPlaceHolder { get; set; } = "Search for a borrower";
+    public string AddNewBook { get; set; } = "Add new book";
+    public string SortBy { get; set; } = "Sort by";
+    public string TitleAsc { get; set; } = "Title ascending \u2191";
+    public string TitleDesc { get; set; } = "Title descending \u2193";
+    public string AuthorAsc { get; set; } = "Author name ascending \u2191";
+    public string AuthorDesc { get; set; } = "Author name descending \u2193";
+    public string NameAsc { get; set; } = "Name ascending \u2191";
+    public string NameDesc { get; set; } = "Name descending \u2193";
+    public string ActiveLoansAsc { get; set; } = "Active loans ascending \u2191";
+    public string ActiveLoansDesc { get; set; } = "Active loans descending \u2193";
+    public string Borrowed { get; set; } = "Borrowed";
+    public string NotBorrowed { get; set; } = "Not borrowed";
+    public string ShowAll { get; set; } = "Show all";
+    public string Filter { get; set; } = "Filter";
+    public string Home { get; set; } = "Home";
+    public string BookShelf { get; set; } = "Bookshelf";
+    public string Borrowers { get; set; } = "Borrowers";
+    public string BorrowedBy { get; set; } = "Borrowed by: ";
+    public string LendOut { get; set; } = "Lend out";
+    public string YourHomeLibrary { get; set; } = "Your home library";
+    public string SwedishLanguage { get; set; } = "Swedish";
+    public string EnglishLanguage { get; set; } = "English";
+    public string Update { get; set; } = "Update";
+    public string Delete { get; set; } = "Delete";
+    public string Return { get; set; } = "Return";
+    public string BookDetails { get; set; } = "Book details";
+    public string UpdateTitlePlaceHolder { get; set; } = "Update title";
+    public string UpdateAuthorPlaceHolder { get; set; } = "Update author name(s)";
+    public string AddBorrower { get; set; } = "Add new borrower";
+    public string Name { get; set; } = "Name";
+    public string Mobile { get; set; } = "Mobile";
+    public string Email { get; set; } = "Email";
+    public string Optional { get; set; } = "Optional";
+    public string UpdateName { get; set; } = "Update name";
+    public string UpdatePhoneNo { get; set; } = "Update phone number";
+    public string UpdateEmail { get; set; } = "Update email";
+    public string BooksBorrowed { get; set; } = "Books borrowed";
+    public string SelectBooksToReturn { get; set; } = "Select books to return";
+    public string BorrowerDetails { get; set; } = "Borrower details";
+    public string PickBooksToLendOut { get; set; } = "Pick books to lend out";
+    public string ReturnBy { get; set; } = "Return by";
+    public string LoanSuccessful { get; set; } = "Loan successful";
+    
+    public string DeleteBorrower { get; set; }
+    public string DeleteBorrowerFailMessage { get; set; }
+    public string DeleteBorrowerSuccessMessage { get; set; }
+    public string BookReturned { get; set; }
+    public string BookReturnedMessage { get; set; }
+    public string DeleteBook { get; set; }
+    public string DeleteBookSuccessMessage { get; set; }
+    public string DeleteBookFailMessage { get; set; }
+    public string GetLoanSuccessFullMessage(int noOfBooks, string borrowerName)
+    {
+        if (noOfBooks == 1)
+        {
+            return $"You lended out {noOfBooks} book to {borrowerName}";
+        }
+        else
+        {
+            return $"You lended out {noOfBooks} books to {borrowerName}";
+        }
+    }
+}
---
+++ ZHomeLibraryShellApp/Language/ILanguage.cs
@@ -0,0 +1,64 @@
+﻿using ZHomeLibraryShellApp.Pages;
+
+namespace ZHomeLibraryShellApp.Language;
+
+public interface ILanguage
+{
+    public string YourBooks { get; set; }
+    public string SearchTitlePlaceHolder { get; set; }
+    public string SearchBorrowerPlaceHolder { get; set; }
+    public string AddNewBook { get; set; }
+    public string SortBy { get; set; }
+    public string TitleAsc { get; set; }
+    public string TitleDesc { get; set; }
+    public string AuthorAsc { get; set; }
+    public string AuthorDesc { get; set; }
+    public string NameAsc { get; set; }
+    public string NameDesc { get; set; }
+    public string ActiveLoansAsc { get; set; }
+    public string ActiveLoansDesc { get; set; }
+    public string Borrowed { get; set; }
+    public string NotBorrowed { get; set; }
+    public string ShowAll { get; set; }
+    public string Filter { get; set; }
+    public string Home { get; set; }
+    public string BookShelf { get; set; }
+    public string Borrowers { get; set; }
+    public string BorrowedBy { get; set; }
+    public string LendOut { get; set; }
+    public string YourHomeLibrary { get; set; }
+    public string SwedishLanguage { get; set; }
+    public string EnglishLanguage { get; set; }
+    public string Update { get; set; }
+    public string Delete { get; set; }
+    public string Return { get; set; }
+    public string BookDetails { get; set; }
+    public string UpdateTitlePlaceHolder { get; set; }
+    public string UpdateAuthorPlaceHolder { get; set; }
+    public string AddBorrower { get; set; }
+    public string Name { get; set; }
+    public string Mobile { get; set; }
+    public string Email { get; set; }
+    public string Optional { get; set; }
+    public string UpdateName { get; set; }
+    public string UpdatePhoneNo { get; set; }
+    public string UpdateEmail { get; set; }
+    public string BooksBorrowed { get; set; }
+    public string SelectBooksToReturn { get; set; }
+    public string BorrowerDetails { get; set; }
+    public string PickBooksToLendOut { get; set; }
+    public string ReturnBy { get; set; }
+    public string LoanSuccessful { get; set; }
+    
+    public string DeleteBorrower { get; set; }
+    public string DeleteBorrowerFailMessage { get; set; }
+    public string DeleteBorrowerSuccessMessage { get; set; }
+    public string BookReturned { get; set; }
+    public string BookReturnedMessage { get; set; }
+    public string DeleteBook { get; set; }
+    public string DeleteBookSuccessMessage { get; set; }
+    public string DeleteBookFailMessage { get; set; }
+
+    public string GetLoanSuccessFullMessage(int noOfBooks, string borrowerName);
+
+}
